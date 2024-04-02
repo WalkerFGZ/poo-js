78cff5351a4adb31d40cf345916c58473daa45cd
Andres Gallego <andresfelipegallegozuleta@gmail.com>
2024-04-01 21:18:10 -0500

    classes

--- main.js
+++ main.js
@@ -24,4 +24,31 @@ Student.prototype.attendCourse = function (courseName) {
 
 const John = new Student('John', 30, ['JS', 'React', 'Node', 'MongoDB'])
 John.attendCourse('Python')
-console.log(John)
+
+
+// prototypes with classes
+
+// class Student2 {
+//     constructor(name, age, courseAttend) { // constructor is a method that runs when we create a new instance of a class
+//         this.name = name;
+//         this.age = age;
+//         this.courseAttend = courseAttend;
+//     }
+//     attendCourse(courseName) { // this method is added to the prototype of the class
+//         this.courseAttend.push(courseName)
+//     }
+// }
+class Student2 {
+    constructor({ name, age, courseAttend }) {  // RORO: Receive and object and return an object
+        this.name = name;
+        this.age = age;
+        this.courseAttend = courseAttend;
+    }
+    attendCourse(courseName) { // this method is added to the prototype of the class
+        this.courseAttend.push(courseName)
+    }
+}
+const John2 = new Student2({ name: 'John', age: 30, courseAttend: ['JS', 'React', 'Node', 'MongoDB'] })
+John2.attendCourse('Vue')
+
+console.log(John2)
